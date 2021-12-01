02d71d194cc0b477b41e48c49989c1cbaa770213
KunigiMohan <kmohanr5@gmail.com>
2021-12-01 17:52:49 +0530

    Added : Second maximum and minimum three digit number in array without sorting.

---
+++ Second_max_min_number.js
@@ -0,0 +1,24 @@
+const array=[];
+for (i=0;i<=9;i++)
+{
+    array[i]=Math.floor(Math.random()*899)+100;     //Inserting random values in array
+}
+for (i=0;i<=9;i++)
+{
+    console.log(array[i]);
+}
+let temp=0;
+for (i=0;i<=9;i++)
+{
+    for(j=0;j<=9;j++){
+    if(array[i]>array[j])                //finding minimum element using if condition
+    {
+        temp=array[i];
+        array[i]=array[j];
+        array[j]=temp;
+        
+    }
+    }
+}
+console.log("Maximum number : "+array[8])                //printing max element 
+console.log("Minimum number : "+array[1]);               //printing min element
